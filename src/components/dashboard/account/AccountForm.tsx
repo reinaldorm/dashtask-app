a73169774b43e537c346e9c2815b2c2fafec723a
Reinaldo Henrique <naldo-mar@hotmail.com>
2023-01-16 05:30:42 -0300

    update

--- src/components/dashboard/account/AccountForm.tsx
+++ src/components/dashboard/account/AccountForm.tsx
@@ -1,62 +1,19 @@
 import React from 'react';
+import AccountField from './AccountField';
 import styles from './css/account.module.css';
-import { updateEmail, updatePassword } from 'firebase/auth';
-
-enum Actions {
-  USERNAME = 'USERNAME',
-  EMAIL = 'EMAIL',
-  PASSWORD = 'PASSWORD',
-}
-
-interface UserCredentials {
-  username: string;
-  email: string;
-  password: string;
-}
-
-interface UserAction {
-  payload: {
-    [key: string]: string;
-  };
-  type: Actions;
-}
 
 interface AccountFormProps {
   data: UserDataInterface;
 }
 
 const AccountForm = ({ data }: AccountFormProps) => {
-  const [credentials, updateCredentials] = React.useReducer(
-    (state: UserCredentials, action: UserAction) => {
-      const { payload, type } = action;
-
-      switch (type) {
-        case Actions.USERNAME:
-          state.username = payload.username;
-          break;
-        case Actions.EMAIL:
-          state.email = payload.email;
-          break;
-        case Actions.PASSWORD:
-          state.password = payload.password;
-          break;
-
-        default:
-          break;
-      }
-
-      return state;
-    },
-    {
-      username: data.username,
-      email: data.email,
-      password: '*************',
-    }
+  return (
+    <div className={styles.accountForm}>
+      <AccountField label="Username" />
+      <AccountField label="Email" />
+      <AccountField label="Password" />
+    </div>
   );
-
-  const handleEdit = () => {};
-
-  return <div className={styles.accountForm}>AccountForm</div>;
 };
 
 export default AccountForm;
